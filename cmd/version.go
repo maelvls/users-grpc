e21e83545073ce473e275c34c3ffddc16d620cb5
Maël Valais <mael.valais@gmail.com>
2019-06-07 17:16:18 +0200

    chore(goreleaser): add windows binaries ftw

--- cmd/version.go
+++ cmd/version.go
@@ -2,19 +2,14 @@ package cmd
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/spf13/cobra"
 )
 
 // AppVersion is passed to cmd.Execute().
 type AppVersion struct {
-	Version    string // 0b5ed7a
-	BuildDate  string // master
-	GitCommit  string // clean or dirty
-	GitBranch  string // v1.0.0, v1.0.1-5-g585c78f-dirty, fbd157c (git describe --tags --dirty --always)
-	GitState   string // 2016-08-04T18:07:54Z
-	GitSummary string // 2.0.0
+	BuildDate  string // 2016-08-04T18:07:54Z
+	GitSummary string // v1.0.0, v1.0.1-5-g585c78f-dirty, fbd157c (git describe --tags --dirty --always)
 }
 
 var versionCmd = &cobra.Command{
@@ -22,12 +17,7 @@ var versionCmd = &cobra.Command{
 	Short: "Print the version of this binary to stdout",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("%s%s\n", version.GitSummary, func() string {
-			if strings.Compare(version.GitBranch, "master") == 0 {
-				return ""
-			}
-			return fmt.Sprintf(" (%s)", version.GitBranch)
-		}())
+		fmt.Printf("%s\n", version.GitSummary)
 	},
 }
 
